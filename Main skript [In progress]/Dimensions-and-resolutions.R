7d01cd98f1a9193c7024586ddc87d57b9bc53a67
GerritSchm <94144425+GerritSchm@users.noreply.github.com>
2023-04-26 17:08:32 -0400

    v1.1

---
+++ Main skript [In progress]/Dimensions-and-resolutions.R
@@ -0,0 +1,68 @@
+#UMAP - Dimensions and Resolutions
+
+#Pre-Subsetting
+
+datasetsUMAP<-c("27M1"= 30,
+                "297_count" = 30,
+                "368_count" = 30,
+                "439_count" = 30,
+                "440_count" = 30,
+                "609_count" = 30,
+                "610_count" = 30,
+                "611_count" = 30,
+                "612_count" = 30,
+                "618_count" = 30,
+                "619_count" = 30,
+                "620_count" = 30,
+                "621_count" = 30,
+                "622_count" = 30)
+
+datasetsClusterRes<-c("27M1"= 0.4,
+                      "297_count" = 0.7,
+                      "368_count" = 0.6,
+                      "439_count" = 0.6,
+                      "440_count" = 0.6,
+                      "609_count" = 0.6,
+                      "610_count" = 0.6,
+                      "611_count" = 0.4,
+                      "612_count" = 0.4,
+                      "618_count" = 0.5,
+                      "619_count" = 0.5,
+                      "620_count" = 0.5,
+                      "621_count" = 0.5,
+                      "622_count" = 0.5)
+
+
+
+
+#Final
+
+datasetsUMAP2<-c("27M1"= 20,
+                 "297_count" = 20,
+                 "368_count" = 20,
+                 "439_count" = 20,
+                 "440_count" = 20,
+                 "609_count" = 25,
+                 "610_count" = 25,
+                 "611_count" = 30,
+                 "612_count" = 30,
+                 "618_count" = 30,
+                 "619_count" = 30,
+                 "620_count" = 30,
+                 "621_count" = 30,
+                 "622_count" = 30)
+
+datasetsClusterRes2<-c("27M1"= 0.5,
+                       "297_count" = 0.4,
+                       "368_count" = 0.3,
+                       "439_count" = 0.3,
+                       "440_count" = 0.3,
+                       "609_count" = 0.5,
+                       "610_count" = 0.5,
+                       "611_count" = 0.4,
+                       "612_count" = 0.4,
+                       "618_count" = 0.4,
+                       "619_count" = 0.4,
+                       "620_count" = 0.4,
+                       "621_count" = 0.4,
+                       "622_count" = 0.4)
